15af9a834fa677799509ef50a28b04bedec8c2f5
Vitaly <v.kononov@chillicode.ru>
2022-07-27 19:26:49 +0300

    added first swagger documentation

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import {Column, DataType, Model, Table} from 'sequelize-typescript'
+import {ApiProperty} from '@nestjs/swagger'
 
 interface UserCreationAttr {
   email: string
@@ -7,12 +8,15 @@ interface UserCreationAttr {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
   @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
   id: number
 
+  @ApiProperty({example: 'user@mail.ru', description: 'Адрес электронной почты'})
   @Column({type: DataType.STRING, unique: true, allowNull: false})
   email: string
 
+  @ApiProperty({example: '12345', description: 'Пароль'})
   @Column({type: DataType.STRING, allowNull: false})
   password: string
 }
